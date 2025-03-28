100e2606fda3cf3532a931649c1b3b35f758816a
aurorakp <alkoch@gmail.com>
2025-03-28 09:51:04 -0400

    asteroids collide with player

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
+from player import Player
+
+def main():
+    pygame.init()
+    game_clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    af = AsteroidField()
+    Player.containers = (updatable, drawable)
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        for ast in asteroids:
+            if ast.is_colliding(p):
+                print('Game over!')
+                sys.exit(0)
+
+
+        screen.fill('black')
+
+        for draw_member in drawable:
+            draw_member.draw(screen)
+       
+        pygame.display.flip()
+
+        tick_time = game_clock.tick(60)
+        dt = tick_time / 1000
+
+        
+if __name__ == "__main__":
+    main()
